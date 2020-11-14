4bf132ab57b5c6926503b5e03f23eda007e437cf
Anish <atechchatterjeee@gmail.com>
2020-11-14 18:52:29 +0530

    change cursor in vi mode

--- omf.fish
+++ omf.fish
@@ -3,23 +3,42 @@ set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
-# changes cursor in insert and normal mode
-#function fish_vi_cursor --on-variable fish_bind_mode
-	#switch $fish_bind_mode
-		#case default
-			#printf '\e]50;CursorShape=0\x7'
-		#case insert
-			#printf '\e]50;CursorShape=1\x7'
-		#case "*"
-			#printf '\e]50;CursorShape=0\x7'
-	#end
-#end
+# fish shell change cursor in vi mode --
+function set_mode_pre_execution --on-event fish_preexec
+    set command (expr $argv : '\([^ ]*\).*')
+    set -g __last_fish_bind_mode $fish_bind_mode
+    if test $command = 'node'
+        or echo $command | grep 'python' >/dev/null ^/dev/null
+    else
+        printf '\e]50;CursorShape=0\x7'
+    end
+end
 
+function set_mode_post_execution --on-event fish_postexec
+    set temp $__last_fish_bind_mode
+    set -e __last_fish_bind_mode
+    set -g fish_bind_mode $temp
+    fish_vi_cursor
+end
+
+function fish_vi_cursor --on-variable fish_bind_mode
+    if set -q __last_fish_bind_mode
+        and test $__last_fish_bind_mode = $fish_bind_mode
+        return
+    end
+    set -g __last_fish_bind_mode $fish_bind_mode
+    switch $fish_bind_mode
+        case insert
+            printf '\e]50;CursorShape=1\x7'
+        case default
+            printf '\e]50;CursorShape=0\x7'
+        case "*"
+            printf '\e]50;CursorShape=0\x7'
+    end
+end
+# --
 
 fish_vi_cursor
-set fish_cursor_insert block
-set fish_cursor_default block
-set fish_cursor_visual block
 
 # removes the fish greeting prompt
 set fish_greeting
@@ -29,18 +48,17 @@ xset r rate 250 100
 set EDITOR "nvim"
 set VISUAL "nvim"
 
+abbr h "cd ~"
 abbr sz "source ~/.zshrc"
 abbr ov "nvim ~/.vimrc"
 abbr of "nvim ~/dotfiles/omf.fish"
 abbr sf "source ~/dotfiles/omf.fish"
-
-abbr h "cd ~"
 abbr systop "systemctl stop docker mysql mongodb apache2"
 abbr weather "curl wttr.in"
 abbr tmuxComp "tmux attach-session -t competitive-coding"
 abbr swallpaper "nitrogen --restore &"
 abbr muteToggle "amixer -q -D pulse set Master toggle"
-abbr showWifi "nmcli d wifi list"
+abbr show_wifi "nmcli d wifi list"
 abbr clock "tty-clock -c -C 1 -b -s -S"
 abbr at "alacritty-theme-switch"
 abbr alc "nvim ~/.config/alacritty/alacritty.yml"
@@ -50,9 +68,10 @@ abbr p "pwd"
 abbr ss "systemctl suspend"
 abbr in "nvim ~/dotfiles/init.vim"
 abbr ns "npm start"
-abbr pys "python3 manage.py runserver"
+abbr pys "python manage.py runserver"
 abbr sys "systemctl"
 abbr change_default_browser "sudo update-alternatives --config x-www-browser"
+abbr db "sudo psql"
 
 alias ls 'exa -al --color=always --group-directories-first' 
 alias la 'exa -a --color=always --group-directories-first' 
@@ -85,8 +104,13 @@ function stop_port
 end
 
 function startpy
+	systemctl start postgresql
 	source ./backend/venv/bin/activate.fish 
-	cd ./backend/src/ && python3 manage.py runserver
+	python ./backend/src/manage.py runserver
+end
+
+function connect_wifi
+	nmcli d wifi connect $argv
 end
 
 # Load Oh My Fish configuration.
