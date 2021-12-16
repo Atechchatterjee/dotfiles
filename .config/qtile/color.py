597e8c06a4032e8c03d08b88d0d17aaf4a96a8e9
atechchatterjee <atechchatterjeee@gmail.com>
2021-12-16 17:32:12 +0530

    adding all the config of qtile wm

---
+++ .config/qtile/color.py
@@ -0,0 +1,35 @@
+# color palete
+colors = {
+    "purple" : "#826BC3",
+    "blue_bg" : "#2bbac5",
+    "white" : "#ffffff",
+    "black" : "#0A0A0A",
+
+    "onedarkBlue" : "#3C909B",
+    "onedarkRed" : "#BE5046",
+
+    "gruvboxRed" : "#F94B3F",
+    "gruvboxAqua" : "#689D6A",
+    "gruvboxBrown" : "#1D2021",
+    "gruvboxOrange" : "#D65F34",
+    "gruvboxBlue" : "#458587",
+
+    "nordRed" : "#BF616A",
+    "nordGreen" : "#A3BE8C",
+    "nordBlue" : "#81A1C1",
+    "nordDBlue" : "#2D333E",
+    "nordPurple" : "#B48EAD",
+    "deepBlue" : "#292d3e",
+    "draculaYellow" : "#F1FA8C",
+    "draculaRed" : "#FF5555",
+    "draculaGreen": "#50FA7B",
+
+    "pt1": "#21243D",
+    "pt2": "#21243D",
+
+    # "icon": "#826BC3",
+    "icon":"#EDEDED",
+    # "icon": "#81A1C1"
+    # "icon": "#C29200",
+    # "icon": "#21243D",
+}
